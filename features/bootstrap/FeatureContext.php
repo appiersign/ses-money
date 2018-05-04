6561fbf64b1700ddc2f131b723591d83584f1a42
Solomon Appier-Sign <solomon@qisimah.com>
2018-05-04 17:06:18 +0000

    behat context tests generated

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,5 +1,6 @@
 <?php
 
+use Behat\Behat\Tester\Exception\PendingException;
 use Behat\Behat\Context\Context;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
@@ -18,5 +19,61 @@ class FeatureContext implements Context
      */
     public function __construct()
     {
+    }
+
+    /**
+     * @Given there is a :arg1, which costs $:arg2
+     */
+    public function thereIsAWhichCosts($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @When I add :arg2 :arg1 to the basket
+     */
+    public function iAddToTheBasket($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 product in the basket
+     */
+    public function iShouldHaveProductInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall price of the basket should be $:arg1
+     */
+    public function theOverallPriceOfTheBasketShouldBe($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Given there is a :arg1, which cost $:arg2
+     */
+    public function thereIsAWhichCost($arg1, $arg2)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then I should have :arg1 products in the basket
+     */
+    public function iShouldHaveProductsInTheBasket($arg1)
+    {
+        throw new PendingException();
+    }
+
+    /**
+     * @Then the overall basket price should be $:arg1
+     */
+    public function theOverallBasketPriceShouldBe($arg1)
+    {
+        throw new PendingException();
     }
 }
