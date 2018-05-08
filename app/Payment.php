08792338083b31cd439f6e3a1196cbf2ecb13ef9
Solomon Appier-Sign <solomon@qisimah.com>
2018-05-08 11:18:11 +0000

    payment controller generated

--- app/Payment.php
+++ app/Payment.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Payment extends Model
 {
-    //
+    protected $fillable = [
+        'merchant_id',
+        'stan',
+        'transaction_id',
+        'provider',
+        'amount',
+        'authorization_code',
+        'response_code',
+        'response_status',
+        'response_message'
+    ];
 }
