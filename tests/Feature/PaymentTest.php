470f46fbbeb54c9062dca3bcfc97c8988610a1c8
Solomon Appier-Sign <solomon@qisimah.com>
2018-05-08 12:03:03 +0000

    test make payment

--- tests/Feature/PaymentTest.php
+++ tests/Feature/PaymentTest.php
@@ -13,8 +13,21 @@ class PaymentTest extends TestCase
      *
      * @return void
      */
-    public function testExample()
+    public function testMakePayment()
     {
-        $this->assertTrue(true);
+        $response = $this->postJson('payment/create', [
+            'merchant_id' => 'SES-000000001',
+            'transaction_id' => time().'00',
+            'description' => 'testing from the other side',
+            'amount' => '000000000010',
+            'provider' => 'MTN'
+        ]);
+        $response->assertJson([
+            'merchant_id' => 'SES-000000001',
+            'transaction_id' => time().'00',
+            'description' => 'testing from the other side',
+            'amount' => '000000000010',
+            'provider' => 'MTN'
+        ]);
     }
 }
