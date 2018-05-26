05f861a156d743ac7be4f1ea6c9c2d2bf94c08bc
Solomon Appier-Sign <solomon@qisimah.com>
2018-05-26 11:23:54 +0000

    routes updated

--- routes/api.php
+++ routes/api.php
@@ -3,26 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('merchant')->group( function() {
-    Route::post('debit', function (Request $request){
-        return $request->all();
-    });
-
-    Route::get('', function (Request $request){
-        return $request->all();
-    });
-
     Route::resource('payments', 'PaymentController');
 });
 
